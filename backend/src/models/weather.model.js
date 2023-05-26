b5f7b532f8fc2077dcb648a100ca249ae10e985f
Leandro <leandrodevlarrea@hotmail.com>
2023-05-26 17:45:41 -0300

    swagger model property, float to number

--- backend/src/models/weather.model.js
+++ backend/src/models/weather.model.js
@@ -3,15 +3,15 @@
  * @typedef {object} Weather
  * @property {string} id.defaul - The location id
  * @property {string} condition.required - The condition (sunny, ranny etc)
- * @property {float}  temperature.required - The temperature i celcius degrees
+ * @property {number}  temperature.required - The temperature i celcius degrees
  * @property {string} cityName.required - The city name
  * @property {string} lat.required - the latitud
  * @property {string} lon.required - the long
- * @property {float}  windMph.required - the wind
- * @property {float}  windDegree.required - the windDegree
- * @property {float}  pressure.required - the pressure
- * @property {float}  feelsLike.required - the feelsLike
- * @property {float}  humidity.required - the humidity
+ * @property {number}  windMph.required - the wind
+ * @property {number}  windDegree.required - the windDegree
+ * @property {number}  pressure.required - the pressure
+ * @property {number}  feelsLike.required - the feelsLike
+ * @property {number}  humidity.required - the humidity
  * @property {string} createdAt - Date of creation - date-time
  * @property {string} updatedAt - Date of last update - date-time
  */
