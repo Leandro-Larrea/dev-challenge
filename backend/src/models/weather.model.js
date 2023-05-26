8de3b20cba0f39be7938a36e1c01a86bc5046e12
Leandro <leandrodevlarrea@hotmail.com>
2023-05-26 16:28:50 -0300

    swagger weather model updated

--- backend/src/models/weather.model.js
+++ backend/src/models/weather.model.js
@@ -7,6 +7,11 @@
  * @property {string} cityName.required - The city name
  * @property {string} lat.required - the latitud
  * @property {string} lon.required - the long
+ * @property {Float}  windMph.required - the wind
+ * @property {Float}  windDegree.required - the windDegree
+ * @property {Float}  pressure.required - the pressure
+ * @property {Float}  feelsLike.required - the feelsLike
+ * @property {Float}  humidity.required - the humidity
  * @property {string} createdAt - Date of creation - date-time
  * @property {string} updatedAt - Date of last update - date-time
  */
